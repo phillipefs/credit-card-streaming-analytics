6410503122a1469562631354c6cc1dc1097db939
Phillipe Santos <phillipefs@msn.com>
2023-09-28 20:49:31 -0300

    convert variables to int

--- app_events.py
+++ app_events.py
@@ -5,11 +5,11 @@ from time import sleep
 
 # Load Variables
 load_dotenv()
-CNN_STRING      = os.getenv("CONN_STRING")
+CNN_STRING      = os.getenv("CNN_STRING_EVENTHUB")
 NAME_EVENTHUB   = os.getenv("NAME_EVENTHUB")
-BATCH_QUANTITY  = os.getenv("BATCH_QUANTITY")
-BATCH_SIZE      = os.getenv("BATCH_SIZE")
-SLEEP_SECONDS   = os.getenv("SLEEP_SECONDS")
+BATCH_QUANTITY  = int(os.getenv("BATCH_QUANTITY"))
+BATCH_SIZE      = int(os.getenv("BATCH_SIZE"))
+SLEEP_SECONDS   = int(os.getenv("SLEEP_SECONDS"))
 
 for _ in range(BATCH_QUANTITY):
     event_hub_streaming = EventHubStreaming(CNN_STRING, NAME_EVENTHUB)
