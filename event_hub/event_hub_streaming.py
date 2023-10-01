0561548a0980dcd3c126e0de18a8a99ebf66cc1a
Phillipe Santos <phillipefs@msn.com>
2023-10-01 11:37:32 -0300

    error handling functions

--- event_hub/event_hub_streaming.py
+++ event_hub/event_hub_streaming.py
@@ -15,35 +15,40 @@ class EventHubStreaming:
         Args: 
             transactions_quantity (int): The number of transactions to generate.
         """
-        fake = Faker()
-        list_transactions = list()
+        try:
+            fake = Faker()
+            list_transactions = list()
 
-        for _ in range(transactions_quantity):
-            #UserInfo        
-            username = fake.user_name()
-            name = fake.name()
-            email = fake.email()
-            city = fake.city()
-            country = fake.country()
+            for _ in range(transactions_quantity):
+                #UserInfo        
+                username = fake.user_name()
+                name = fake.name()
+                email = fake.email()
+                city = fake.city()
+                country = fake.country()
 
-            #CreditCard
-            credit_card_number = fake.credit_card_number()
-            credit_card_provider = fake.credit_card_provider()
-            credit_card_expiration = fake.credit_card_expire()
-            purchase_amount = round(random.uniform(10.0, 1000.0), 2)
+                #CreditCard
+                credit_card_number = fake.credit_card_number()
+                credit_card_provider = fake.credit_card_provider()
+                credit_card_expiration = fake.credit_card_expire()
+                purchase_amount = round(random.uniform(10.0, 1000.0), 2)
 
-            list_transactions.append({
-                'username': username,
-                'name': name,
-                'email': email,
-                'city': city,
-                'country': country,
-                'credit_card_number': credit_card_number,
-                'credit_card_provider': credit_card_provider,
-                'credit_card_expiration': credit_card_expiration,
-                'purchase_amount': purchase_amount
-            })            
-        return list_transactions
+                list_transactions.append({
+                    'username': username,
+                    'name': name,
+                    'email': email,
+                    'city': city,
+                    'country': country,
+                    'credit_card_number': credit_card_number,
+                    'credit_card_provider': credit_card_provider,
+                    'credit_card_expiration': credit_card_expiration,
+                    'purchase_amount': purchase_amount
+                })
+            return list_transactions
+        
+        except Exception as e:
+            print(f"Error: {str(e)}")
+            
     
     def insert_batch_events(self, batch_size:int):
         """
@@ -55,5 +60,12 @@ class EventHubStreaming:
         producer = EventHubProducerClient.from_connection_string(conn_str=self.cnn_string, eventhub_name=self.event_hub_name)
 
         with producer:
-            producer.send_batch(event_data_list)
+            try:
+                producer.send_batch(event_data_list)
+            except ValueError:  # size exceeds limit.
+                print("size of the event data list exceeds the size limit of a single send")
+            except EventHubError as eh_err:
+                print("sending error: ", eh_err)
+            except Exception as e:
+                print(f"Error: {str(e)}")
 
