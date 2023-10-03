0b5994a403a9020ac8908a6c176b948daa0dfd6c
phillipefs@msn.com <phillipefs@msn.com>
2023-10-03 22:00:59 +0000

    create database and delta table cc_transactions

--- databricks/environment_configs/databases_delta_tables.py
+++ databricks/environment_configs/databases_delta_tables.py
@@ -10,27 +10,25 @@
 # DBTITLE 1,Databases
 # MAGIC %sql
 # MAGIC
-# MAGIC CREATE DATABASE IF NOT EXISTS analytics_bronze LOCATION "/mnt/layer-bronze/databricks";
-# MAGIC CREATE DATABASE IF NOT EXISTS analytics_silver LOCATION "/mnt/layer-silver/databricks";
-# MAGIC CREATE DATABASE IF NOT EXISTS analytics_gold   LOCATION "/mnt/layer-gold/databricks";
+# MAGIC CREATE DATABASE IF NOT EXISTS analytics_bronze LOCATION "/mnt/layer-bronze/databricks/analytics_bronze";
+# MAGIC CREATE DATABASE IF NOT EXISTS analytics_silver LOCATION "/mnt/layer-silver/databricks/analytics_silver";
+# MAGIC CREATE DATABASE IF NOT EXISTS analytics_gold   LOCATION "/mnt/layer-gold/databricks/analytics_gold";
 
 # COMMAND ----------
 
 # DBTITLE 1,Create Delta Table Raw
 # MAGIC %sql
-# MAGIC create table analytics_bronze.teste ( id int) location "/mnt/layer-bronze/databricks/teste";
-
-# COMMAND ----------
-
-# MAGIC %sql
-# MAGIC insert into analytics_bronze.teste values(1)
-
-# COMMAND ----------
-
-# MAGIC %sql
-# MAGIC show databases
-
-# COMMAND ----------
-
-# MAGIC %sql 
-# MAGIC select count(*) from analytics_bronze.transactions_credit_card
+# MAGIC create table analytics_bronze.cc_transactions(
+# MAGIC   username STRING,
+# MAGIC   name STRING,
+# MAGIC   email STRING,
+# MAGIC   city STRING,
+# MAGIC   country STRING,
+# MAGIC   credit_card_number STRING,
+# MAGIC   credit_card_provider STRING,
+# MAGIC   credit_card_expiration STRING,
+# MAGIC   purchase_amount STRING,
+# MAGIC   event_timestamp STRING
+# MAGIC )
+# MAGIC USING DELTA
+# MAGIC LOCATION "/mnt/layer-bronze/databricks/analytics_bronze/cc_transactions";
