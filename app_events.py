493962d243849a985c1ab6a1d18281806bc7f16a
Phillipe Santos <phillipefs@msn.com>
2023-10-14 13:56:59 -0300

    convert functions to async

--- app_events.py
+++ app_events.py
@@ -1,17 +1,27 @@
 import os
 from dotenv import load_dotenv
 from event_hub.event_hub_streaming import EventHubStreaming
-from time import sleep
+import asyncio
 
 # Load Variables
 load_dotenv()
-CNN_STRING      = os.getenv("CNN_STRING_EVENTHUB")
-NAME_EVENTHUB   = os.getenv("NAME_EVENTHUB")
-BATCH_QUANTITY  = int(os.getenv("BATCH_QUANTITY"))
-BATCH_SIZE      = int(os.getenv("BATCH_SIZE"))
-SLEEP_SECONDS   = int(os.getenv("SLEEP_SECONDS"))
+CNN_STRING = os.getenv("CNN_STRING_EVENTHUB")
+NAME_EVENTHUB = os.getenv("NAME_EVENTHUB")
+BATCH_QUANTITY = int(os.getenv("BATCH_QUANTITY"))
+BATCH_SIZE = int(os.getenv("BATCH_SIZE"))
+SLEEP_SECONDS = int(os.getenv("SLEEP_SECONDS"))
 
-for _ in range(BATCH_QUANTITY):
-    event_hub_streaming = EventHubStreaming(CNN_STRING, NAME_EVENTHUB)
-    event_hub_streaming.insert_batch_events(BATCH_SIZE)
-    sleep(SLEEP_SECONDS)
+async def insert_batches():
+    tasks = []
+    try:
+        for _ in range(BATCH_QUANTITY):
+            event_hub_streaming = EventHubStreaming(CNN_STRING, NAME_EVENTHUB)
+            tasks.append(event_hub_streaming.insert_batch_events(BATCH_SIZE))
+
+        await asyncio.gather(*tasks)
+        print("All batches inserted successfully.")
+        
+    except Exception as e:
+        print(f"Error during batch insertion: {e}")
+
+asyncio.run(insert_batches())
--- event_hub/event_hub_streaming.py
+++ event_hub/event_hub_streaming.py
@@ -1,18 +1,18 @@
-from azure.eventhub import EventHubProducerClient, EventData
+from azure.eventhub import EventData
+from azure.eventhub.aio import EventHubProducerClient
 from azure.eventhub.exceptions import EventHubError
 from faker import Faker
 import random
-
 
 class EventHubStreaming:
     def __init__(self, cnn_string, event_hub_name) -> None:
         self.cnn_string = cnn_string
         self.event_hub_name = event_hub_name
 
-    def generate_transactions_cc(self, transactions_quantity:int) -> list:
+    def generate_transactions_cc(self, transactions_quantity: int) -> list:
         """
         Generates a list of mock credit card transactions with random data.
-        Args: 
+        Args:
             transactions_quantity (int): The number of transactions to generate.
         """
         try:
@@ -20,14 +20,14 @@ class EventHubStreaming:
             list_transactions = list()
 
             for _ in range(transactions_quantity):
-                #UserInfo        
+                # UserInfo
                 username = fake.user_name()
                 name = fake.name()
                 email = fake.email()
                 city = fake.city()
                 country = fake.country()
 
-                #CreditCard
+                # CreditCard
                 credit_card_number = fake.credit_card_number()
                 credit_card_provider = fake.credit_card_provider()
                 credit_card_expiration = fake.credit_card_expire()
@@ -45,27 +45,25 @@ class EventHubStreaming:
                     'purchase_amount': purchase_amount
                 })
             return list_transactions
-        
+
         except Exception as e:
             print(f"Error: {str(e)}")
-            
-    
-    def insert_batch_events(self, batch_size:int):
+
+    async def insert_batch_events(self, batch_size: int):
         """
         Inserts a batch of events into the Azure Event Hub.
         Args:
-            batch_size (int): The number of events to generate and insert into the Event Hub. 
+            batch_size (int): The number of events to generate and insert into the Event Hub.
         """
         event_data_list = [EventData(format(i)) for i in self.generate_transactions_cc(batch_size)]
-        producer = EventHubProducerClient.from_connection_string(conn_str=self.cnn_string, eventhub_name=self.event_hub_name)
 
-        with producer:
+        producer = EventHubProducerClient.from_connection_string(conn_str=self.cnn_string, eventhub_name=self.event_hub_name)
+        async with producer:
             try:
-                producer.send_batch(event_data_list)
+                await producer.send_batch(event_data_list)
             except ValueError:  # size exceeds limit.
                 print("size of the event data list exceeds the size limit of a single send")
             except EventHubError as eh_err:
                 print("sending error: ", eh_err)
             except Exception as e:
                 print(f"Error: {str(e)}")
-
